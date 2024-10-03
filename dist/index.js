91ed6ad34a95e5fdb5ee21f3d10867077fc6e01c
rs22095L.nago4 <120536850+nago4@users.noreply.github.com>
2024-10-03 20:13:48 +0900

    first commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// console.log("■ prac00.ts の実行");
+// import "./prac00";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("■ prac01.ts の実行");
+require("./prac01");
---
+++ src/prac01.ts
@@ -0,0 +1,3 @@
+export {};
+const userName: string = "Bob";
+console.log(`Hey, ${userName}.`);
